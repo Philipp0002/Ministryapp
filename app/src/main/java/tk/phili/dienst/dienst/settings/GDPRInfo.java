83ba2963e928719ba8c5c6110f1b70486efd7496
Philipp0002 <philipp@raffaelhahn.de>
2023-03-19 20:31:59 +0100

    Fix GDPR info screen

--- app/src/main/java/tk/phili/dienst/dienst/settings/GDPRInfo.java
+++ app/src/main/java/tk/phili/dienst/dienst/settings/GDPRInfo.java
@@ -2,9 +2,13 @@ package tk.phili.dienst.dienst.settings;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +16,8 @@ import tk.phili.dienst.dienst.R;
 import tk.phili.dienst.dienst.Splash;
 
 public class GDPRInfo extends AppCompatActivity {
+
+    private static final String GDPR_URL = "https://ministryapp.de/privacy";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,27 @@ public class GDPRInfo extends AppCompatActivity {
         }
 
         WebView web = findViewById(R.id.webView);
-        web.loadUrl("https://ministryapp.de/privacy");
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        web.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public void doUpdateVisitedHistory(WebView view, String url, boolean isReload) {
+                if(!url.equals(GDPR_URL)) {
+                    web.loadUrl(GDPR_URL);
+                }
+                super.doUpdateVisitedHistory(view, url, isReload);
+            }
+        });
+
+        web.loadUrl(GDPR_URL);
 
         findViewById(R.id.accept_dsgvo).setOnClickListener(view -> {
             editor.putBoolean("dsgvo_accept", true);
